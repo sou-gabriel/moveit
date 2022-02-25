8cd8e09ae1857d6ec77c3c2c2fe7013716a5fc8d
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2022-02-25 12:01:58 -0300

    Create app colors theme

---
+++ styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primaryBlue: '#5965E0',
+    secondaryBlue: '#4953B8',
+    green: '#4CD62B',
+    red: '#E83F5B',
+    white: '#FFFFFF',
+    text: '#666666',
+    textHighlight: '#B2B9FF',
+    title: '#2E384D',
+    grayLine: '#DCDDE0',
+    background: '#F2F3F5',
+  },
+}
