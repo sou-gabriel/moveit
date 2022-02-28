47417ee7e340cb87fc75ecb3db75a940ef68c314
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2022-02-28 17:58:26 -0300

    Configure @svgr to import SVG's as components

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+  images: {
+    domains: ['github.com'],
+  },
 }
 
 module.exports = nextConfig
