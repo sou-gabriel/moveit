711c70a25038bed9dc198b01dad862ad05a67180
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2022-03-04 15:51:35 -0300

    Create logout functionality

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -1,10 +1,11 @@
+import { signOut } from 'next-auth/react'
 import { CgLogOut } from 'react-icons/cg'
 
 import { Container } from './styles'
 
 export const LogoutButton = () => {
   return (
-    <Container type="button" onClick={() => {}}>
+    <Container type="button" onClick={() => signOut()}>
       <CgLogOut size={36} color="#5965E0" />
     </Container>
   )
